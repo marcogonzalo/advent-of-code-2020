6bb0c680e40c11b348ffb7ab6774abb60df0c437
Marco Gonzalo Gómez Pérez <marcogonzalo@gmail.com>
2020-12-04 21:41:16 +0000

    Day 01

---
+++ Day-01_Report-Repair/app.py
@@ -0,0 +1,16 @@
+import functools 
+import input
+
+def entriesThatSum2020(arr):
+    n = len(arr)
+    for a in range(n):
+        for b in range(a+1,n):
+            rest = 2020 - (arr[a] + arr[b])
+            for c in range(b+1,n):
+                if arr[c] == rest:
+                    return arr[a], arr[b], arr[c]
+    return false
+
+values = entriesThatSum2020(input.values)
+print("2020 =", " + ".join(str(x) for x in values))
+print("Product:", functools.reduce(lambda a,b : a*b, values)) 
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# advent-of-code-2020
+# Advent of code 2020
+
+[Day 1: Report Repair] (https://adventofcode.com/2020/day/1)
